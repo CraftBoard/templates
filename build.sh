4ca49d18eee25caf5c3f81fd79f81bbb043dc3f2
panmourovaty <35844848+panmourovaty@users.noreply.github.com>
2023-01-09 20:59:11 +0100

    increase compression

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ apt-get update && apt-get install -y zstd wget
 
 wget 'https://api.purpurmc.org/v2/purpur/1.19.3/latest/download' -O ./Vannila-Purpur-latest/server/server.jar
 cd ./Vannila-Purpur-latest
-tar --owner=0 --group=0 -I "zstd -T0 -19" -cf ../Vannila-Purpur-latest.tar.zst *
+tar --owner=0 --group=0 -I "zstd -T0 --ultra -22" -cf ../Vannila-Purpur-latest.tar.zst *
 cd ../

7e874cd5bb4d5d1e377cb9e5b769ecb38798ebb9
panmourovaty <35844848+panmourovaty@users.noreply.github.com>
2023-01-09 20:24:26 +0100

    add archive creation

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 wget 'https://api.purpurmc.org/v2/purpur/1.19.3/latest/download' -O ./Vannila-Purpur-latest/server/server.jar
+tar --zstd -cf Vannila-Purpur-latest.tar.zst ./Vannila-Purpur-latest/*

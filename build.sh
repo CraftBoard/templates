1f203be55172e2813851dcbcd749542211cdb1f8
panmourovaty <35844848+panmourovaty@users.noreply.github.com>
2023-01-09 20:36:52 +0100

    add zstd adnd wget

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+apt-get update && apt-get install -y zstd wget
+
 wget 'https://api.purpurmc.org/v2/purpur/1.19.3/latest/download' -O ./Vannila-Purpur-latest/server/server.jar
 tar --zstd -cf Vannila-Purpur-latest.tar.zst ./Vannila-Purpur-latest/*

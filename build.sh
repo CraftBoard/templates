93fef3fcfc0243f4071e1965ec6135b755948a96
panmourovaty <35844848+panmourovaty@users.noreply.github.com>
2023-01-09 20:53:56 +0100

    improve tar command

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 apt-get update && apt-get install -y zstd wget
 
 wget 'https://api.purpurmc.org/v2/purpur/1.19.3/latest/download' -O ./Vannila-Purpur-latest/server/server.jar
-tar --zstd -cf Vannila-Purpur-latest.tar.zst ./Vannila-Purpur-latest/*
+cd ./Vannila-Purpur-latest
+tar --owner=0 --group=0 -I "zstd -T0 -19" -cf Vannila-Purpur-latest.tar.zst *
